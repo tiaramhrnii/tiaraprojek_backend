3294d9cc6175eec74de699c8f588c13956980e9f
tiaramhrnii <tiararara488@gmail.com>
2026-02-25 19:59:20 +0700

    Tambah folder backend

--- update.php
+++ update.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Content-Type: application/json");
 include 'koneksi.php';
 
